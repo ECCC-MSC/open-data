c83fdc14c1b706e40d4f49917953379b55174712
Gabriel Desharnais <gabriel.desharnais2@canada.ca>
2017-11-10 18:47:16 +0000

    Changing virtual-env to python3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
 # =================================================================
 #
-# Terms and Conditions of Use
-#
-# Unless otherwise noted, computer program source code of this
-# distribution is covered under Crown Copyright, Government of
-# Canada, and is distributed under the MIT License.
-#
-# The Canada wordmark and related graphics associated with this
-# distribution are protected under trademark law and copyright law.
-# No permission is granted to use them outside the parameters of
-# the Government of Canada's corporate identity program. For
-# more information, see
-# http://www.tbs-sct.gc.ca/fip-pcim/index-eng.asp
-#
-# Copyright title to all 3rd party software distributed with this
-# software is held by the respective copyright holders as noted in
-# those files. Users are asked to read the 3rd Party Licenses
-# referenced with those assets.
-#
 # Copyright (c) 2017 Government of Canada
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # =================================================================
 
 PYTHON=python-package
 
-virtualenv ${PYTHON}
+virtualenv -p python3 ${PYTHON}
 
 source ${PYTHON}/bin/activate
 
